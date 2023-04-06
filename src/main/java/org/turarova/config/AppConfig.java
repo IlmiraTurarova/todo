1689433a4780ebe3da1a719367d469353e06f95c
IlmiraTurarova <ilmira.rita@gmail.com>
2023-04-06 20:33:11 +0600

    Docker compose

--- src/main/java/org/turarova/config/AppConfig.java
+++ src/main/java/org/turarova/config/AppConfig.java
@@ -38,7 +38,8 @@ public class AppConfig {
     public DataSource dataSource() {
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setDriverClassName("com.p6spy.engine.spy.P6SpyDriver");
-        dataSource.setJdbcUrl("jdbc:p6spy:mysql://localhost:3306/todo");
+        //instead of localhost use db
+        dataSource.setJdbcUrl("jdbc:p6spy:mysql://db:3306/todo");
         dataSource.setUsername("root");
         dataSource.setPassword("mysql-root-password");
         dataSource.setMaximumPoolSize(10);
